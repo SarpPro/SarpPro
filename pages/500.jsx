5cfb9b72146dce9ae5d95ab2319d234f8d83de50
Sarp <160330791+SarpPro@users.noreply.github.com>
2024-02-17 18:01:04 +0300

    Delete pages directory

--- pages/500.jsx
+++
@@ -1,6 +0,0 @@
-import ErrorPage from '../components/ErrorPage';
-const UnkownPage = () => {
-    return <ErrorPage code={500} message={"Oh. Sorry Server Error Please Try Again Later!"} />
-}
-
-export default UnkownPage;
