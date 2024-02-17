902ae555e7820396ae57443a7f86053b684285e2
Sarp <160330791+SarpPro@users.noreply.github.com>
2024-02-17 18:03:41 +0300

    Add files via upload

---
+++ pages/index.jsx
@@ -0,0 +1,99 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Index() {
+	
+  return (
+    <>
+      <div className="py-20 mb-30 max-w-3xl mx-auto">
+        <div className="flex w-full justify-center">
+      <img width="470" src="/img/WebPp.png" />
+      </div>
+        <p className="animateHeader text-4xl font-extrabold text-center text-white">
+        </p>
+        <p className="animateHeader text-white text-opacity-60 text-center mt-0">
+        Many Advanced Systems and More...
+	  </p>
+        <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
+  
+            <Link href={"https://discord.com/oauth2/authorize?client_id=1104853009265086464&scope=bot%20applications.commands&permissions=379969"}>
+              <a
+                className={
+                  "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
+                }
+              >
+          Add to Your Server
+              </a>
+            </Link>
+  <div className="py-10"></div>
+  <Link href={"https://discord.gg/owoyam"}>
+  <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
+  Support Server </a>
+  </Link>
+        </div>
+
+        <div style={{ zIndex: '-1' }} className="hidden xl:block undrag pointer-events-none opacity-10 -left-[10vw] absolute top-[8vw] h-[35vw] flex-shrink-0 grayscale rotate-[1deg]">
+        <Image width="600" height="600" src="/img/Dc1.png"  />
+        </div>
+        <div style={{ zIndex: '-1' }} className="hidden xl:block undrag opacity-10 pointer-events-none -right-[15vw] absolute top-[13vw] h-[90vw] grayscale -rotate-9">
+        <Image  width="600" height="600" src="/img/Dc2.png"  />
+        </div>
+      </div>
+
+
+
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
+          <div className="col-span-4">
+            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"SarpPro" }} />
+            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "Try SarpPro, which entertains you with its commands, now!" }}/>
+            <a href="https://discord.com/oauth2/authorize?client_id=1104853009265086464&scope=bot%20applications.commands&permissions=379969" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+              <i className="fab fa-discord mr-2" /> Add SarpPro to Your Server!
+            </a>
+          </div>
+
+          <div className="col-span-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
+           
+              <div>
+                <div className="text-white">
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> About Bot </p>
+                  <p className="text-gray-500 line-clamp-4"> SarpPro, whose construction started in 2023 and whose beta was presented in 2023, will serve you with 41+ commands in 2024.</p>
+                </div>
+              </div>
+              <div>
+                <div className="text-white">
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-universal-access text-2xl text-amber-500`} /> Accessibility </p>
+                  <p className="text-gray-500 line-clamp-4"> You can easily access and start using all commands of SarpPro using (C). If you want to try it now, add it to your server!</p>
+                </div>
+              </div>
+              <div>
+                <div className="text-white">
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-shield text-2xl text-amber-500`} /> Security </p>
+                  <p className="text-gray-500 line-clamp-4"> You can easily access and start using all commands of SarpPro using (C). If you want to try it now, add it to your server! </p>
+                </div>
+              </div>
+              <div>
+                <div className="text-white">
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-ticket text-2xl text-amber-500`} /> Help & question </p>
+                  <p className="text-gray-500 line-clamp-4"> Don't hesitate to ask when you need help with SarpPro. You can ask your questions by coming to our Discord Server.</p>
+                </div>
+              </div>
+          </div>
+      </div>
+
+
+
+      <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
+              <>
+                <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
+                  <img src=" TEL İÇİN RESİM "  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
+                  <p className="text-3xl text-white font-semibold"> Your Server's Best Bot! </p>
+                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5"> Add SarpPro to your server and enjoy.  </p>
+                </div>
+                <img src=" PC İÇİN RESİM " className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+              </>
+      
+        </div>
+
+      </>
+  )
+}
